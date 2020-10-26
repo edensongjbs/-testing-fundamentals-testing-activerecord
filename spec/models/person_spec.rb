39e84054dcaef87c134ab42a753801c0763b4a22
edensongjbs <edensongjbs@gmail.com>
2020-10-26 10:02:42 -0400

    add validation testing

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -3,7 +3,11 @@ require "spec_helper"
 describe Person do
   it "requires a name to be valid" do
     # Write your spec here.
-    # You can delete the pending line.
-    pending "Not written yet"
+    # You can delete the pending line
+    person1 = Person.new(first_name:"Josh")
+    person2 = Person.new(first_name:"")
+    expect(person1).to be_valid
+    expect(person2).to_not be_valid
+    # pending "Not written yet"
   end
 end
