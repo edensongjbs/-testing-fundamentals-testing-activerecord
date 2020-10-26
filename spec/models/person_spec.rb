3c8c5fa7e7beff9bc9a34f1e746115ada111815f
edensongjbs <edensongjbs@gmail.com>
2020-10-26 12:06:57 -0400

    rev tests

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,13 +1,23 @@
 require "spec_helper"
 
 describe Person do
-  it "requires a name to be valid" do
+  it "is invalid when first_name is nil" do
+    person3 = Person.new()
+    expect(person3).to be_invalid
+  end
+
+  it 'is invalid when first_name is ""' do
+    person2 = Person.new(first_name:"")
+    expect(person2).to_not be_valid
+  end
+
+  it 'is valid when first_name is not empty' do
     # Write your spec here.
     # You can delete the pending line
     person1 = Person.new(first_name:"Josh")
-    person2 = Person.new(first_name:"")
+    
     expect(person1).to be_valid
-    expect(person2).to_not be_valid
+    
     # pending "Not written yet"
   end
 end
